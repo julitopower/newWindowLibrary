938bcf5d385e5a0c50787eb603ba638a71ae3958
Julio Delgado Mangas <julio.delgadomangas@epfl.com>
2014-02-02 10:13:38 +0100

    Adding GPLv3 licence artifacts.

--- include/EGLWindow.h
+++ include/EGLWindow.h
@@ -1,4 +1,22 @@
 /*
+ * Simple Raspberry Pi OpenGl-ES window tests application 
+ * Copyright (C) 2013-2014, Julio Delgado <julio.delgadomangas@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
   Copyright (C) 2012 Jon Macey
 
     This program is free software: you can redistribute it and/or modify
